e160ac5a7178d6da03bdd031b12e7cd08ff34cf7
angeltrevinov <angel.trevinov@gmail.com>
2025-05-05 22:04:07 -0700

    feat(Notes): added editor with editor.js

---
+++ src/routes/markdown-editor/+page.svelte
@@ -0,0 +1,30 @@
+
+<script lang="ts">
+    import { onMount } from 'svelte';
+
+    onMount(async () => {
+        const EditorJS = (await import('@editorjs/editorjs')).default;
+        const Header = (await import('@editorjs/header')).default;
+        const editor = new EditorJS({
+            holder: 'editorjs',
+            tools: {
+                header: {
+                    class: Header,
+                    config: {
+                        placeholder: 'Enter a header',
+                        levels: [1, 2, 3, 4],
+                        defaultLevel: 1,
+                    },
+                },
+            },
+        });
+    });
+</script>
+
+<h1 class="text-3xl font-bold underline">
+    Markdown Editor Exploration
+</h1>
+
+<div id="editorjs">
+
+</div>
